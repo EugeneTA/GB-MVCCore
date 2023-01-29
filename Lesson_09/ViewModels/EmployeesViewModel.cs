9c76ae763c4e03e2b090f7f72bd7a1fdb2d5f161
EugeneTA <tishkov2005@yandex.ru>
2023-01-29 23:21:05 +0300

    Lesson_09 Homework

---
+++ Lesson_09/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Lesson09.ViewModels
+{
+    public class EmployeesViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
